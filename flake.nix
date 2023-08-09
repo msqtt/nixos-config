d5c8bb661ddca802c9aa2326f3888fee9eb68e7a
mosquito <mosquito@email.cn>
2023-08-09 16:59:40 +0800

    fix: warning-defaultPriority

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.05";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
@@ -20,7 +20,6 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-
             home-manager.users.alice = import ./home/alice;
 
             # 使用 home-manager.extraSpecialArgs 自定义传递给 ./home.nix 的参数
