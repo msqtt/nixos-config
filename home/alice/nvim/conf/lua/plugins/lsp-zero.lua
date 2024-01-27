bfe76f106bb87e5c1b499200eb99d71987909610
msqtt <mosquito@email.cn>
2024-01-27 14:23:31 +0800

    update zero-lsp and add leetcode

--- home/alice/nvim/conf/lua/plugins/lsp-zero.lua
+++ home/alice/nvim/conf/lua/plugins/lsp-zero.lua
@@ -1,130 +1,155 @@
-local confn = function()
-  local lsp = require('lsp-zero').preset({})
+return {
+  {
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v3.x',
+    lazy = true,
+    config = false,
+    init = function()
+      -- Disable automatic setup, we are doing it manually
+      vim.g.lsp_zero_extend_cmp = 0
+      vim.g.lsp_zero_extend_lspconfig = 0
+    end,
+  },
 
-  lsp.on_attach(function(client, bufnr)
-    -- see :help lsp-zero-keybindings
-    -- to learn the available actions
-    lsp.default_keymaps({ buffer = bufnr, preserve_mappings = false })
-    local opts = { buffer = bufnr }
-
-    vim.keymap.set({ 'n', 'x' }, 'gn', function()
-      vim.lsp.buf.rename()
-    end, opts)
-    vim.keymap.set({ 'n', 'x' }, 'gq', function()
-      vim.lsp.buf.format({ async = false, timeout_ms = 10000 })
-    end, opts)
-    vim.keymap.set({ 'n', 'x' }, 'ga', function()
-      vim.lsp.buf.code_action()
-    end, opts)
-  end)
-
-  -- setup lsp
-  lsp.setup_servers({
-    'tsserver',
-    'eslint',
-    'html',
-    'cssls',
-    'tailwindcss',
-    'volar',
-    'gopls',
-    'pyright',
-    'rnix',
-    'rust_analyzer',
-    'texlab',
-    'hls',
-    'bufls',
-    'typst_lsp',
-    'sqlls'
-  })
-
-  -- (Optional) Configure lua language server for neovim
-  require 'lspconfig'.lua_ls.setup(lsp.nvim_lua_ls())
-  require 'lspconfig'.volar.setup {
-    init_options = {
-      typescript = {
-        tsdk = '/nix/store/yi14ijlcd02g1nsrf6izbhnwj9vj2lxx-typescript-5.1.6/lib/node_modules/typescript/lib'
-        -- Alternative location if installed as root:
-        -- tsdk = '/usr/local/lib/node_modules/typescript/lib'
-      }
-    }
-  }
-
-  lsp.setup()
-
-  -- nvim-cmp configurations
-  local cmp = require('cmp')
-  local cmp_action = require('lsp-zero').cmp_action()
-  local cmp_format = require('lsp-zero').cmp_format()
-
-  require('luasnip.loaders.from_vscode').lazy_load() -- for friendly snippets.
-
-  cmp.setup({
-    window = {
-      completion = cmp.config.window.bordered(winhighlight),
-      documentation = cmp.config.window.bordered(winhighlight),
-    },
-    sources = {
-      { name = 'buffer' },
-      { name = 'path' },
-      { name = 'nvim_lsp' },
-      { name = 'luasnip' },
-    },
-    mapping = {
-      -- `Enter` key to confirm completion
-      ['<CR>'] = cmp.mapping.confirm({ select = true }),
-
-      -- Ctrl+Space to trigger completion menu
-      ['<C-Space>'] = cmp.mapping.complete(),
-
-      -- Navigate between snippet placeholder
-      ['<C-l>'] = cmp_action.luasnip_jump_forward(),
-      ['<C-k>'] = cmp_action.luasnip_jump_backward(),
-      ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-      ['<C-d>'] = cmp.mapping.scroll_docs(4),
-    },
-    --- (Optional) Show source name in completion menu
-    formatting = cmp_format,
-  })
-  -- `/` cmdline setup.
-  cmp.setup.cmdline('/', {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = {
-      { name = 'buffer' }
-    }
-  })
-  -- `:` cmdline setup.
-  cmp.setup.cmdline(':', {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = cmp.config.sources({
-      { name = 'path' }
-    }, {
+  -- Autocompletion
+  {
+    'hrsh7th/nvim-cmp',
+    event = 'InsertEnter',
+    dependencies = {
       {
-        name = 'cmdline',
-        option = {
-          ignore_cmds = { 'Man', '!' }
+        'L3MON4D3/LuaSnip',
+        "rafamadriz/friendly-snippets",
+        'hrsh7th/cmp-buffer',
+        'hrsh7th/cmp-path',
+        'hrsh7th/cmp-cmdline'
+      },
+    },
+    config = function()
+      -- Here is where you configure the autocompletion settings.
+      local lsp_zero = require('lsp-zero')
+      lsp_zero.extend_cmp()
+
+      -- And you can configure cmp even more, if you want to.
+      local cmp = require('cmp')
+      local cmp_action = lsp_zero.cmp_action()
+
+      cmp.setup({
+        formatting = lsp_zero.cmp_format(),
+        mapping = cmp.mapping.preset.insert({
+          ['<CR>'] = cmp.mapping.confirm({ select = false }),
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-d>'] = cmp.mapping.scroll_docs(4),
+          ['<C-k>'] = cmp_action.luasnip_jump_forward(),
+          ['<C-l>'] = cmp_action.luasnip_jump_backward(),
+        }),
+        window = {
+          completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
+        },
+        sources = {
+          { name = 'buffer' },
+          { name = 'path' },
+          { name = 'nvim_lsp' },
+          { name = 'luasnip' },
+        },
+      })
+      -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+      cmp.setup.cmdline({ '/', '?' }, {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = 'buffer' }
+        }
+      })
+
+      -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+      cmp.setup.cmdline(':', {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = 'path' }
+        }, {
+          { name = 'cmdline' }
+        })
+      })
+    end
+  },
+
+  -- LSP
+  {
+    'neovim/nvim-lspconfig',
+    cmd = 'LspInfo',
+    event = { 'BufReadPre', 'BufNewFile' },
+    dependencies = {
+      { 'hrsh7th/cmp-nvim-lsp' },
+    },
+    config = function()
+      -- This is where all the LSP shenanigans will live
+      local lsp_zero = require('lsp-zero')
+      lsp_zero.extend_lspconfig()
+
+      lsp_zero.on_attach(function(client, bufnr)
+        -- see :help lsp-zero-keybindings
+        -- to learn the available actions
+        lsp_zero.default_keymaps({
+          buffer = bufnr,
+
+          preserve_mappings = false
+        })
+        local opts = { buffer = bufnr }
+
+        vim.keymap.set({ 'n', 'x' }, 'gn', function()
+          vim.lsp.buf.rename()
+        end, opts)
+        vim.keymap.set({ 'n', 'x' }, 'gq', function()
+          vim.lsp.buf.format({ async = false, timeout_ms = 10000 })
+        end, opts)
+        vim.keymap.set({ 'n', 'x' }, 'ga', function()
+          vim.lsp.buf.code_action()
+        end, opts)
+      end)
+
+      lsp_zero.format_on_save({
+        format_opts = {
+          async = false,
+          timeout_ms = 10000,
+        },
+        servers = {
+          ['lua_ls'] = { 'lua' },
+          ['gopls'] = { 'go' },
+          ['tsserver'] = { 'javascript', 'typescript' },
+          ['rust_analyzer'] = { 'rust' },
+        }
+      })
+
+      lsp_zero.setup_servers({
+        'tsserver',
+        'eslint',
+        'html',
+        'cssls',
+        'tailwindcss',
+        'volar',
+        'gopls',
+        'pyright',
+        'rnix',
+        'rust_analyzer',
+        'texlab',
+        'hls',
+        'bufls',
+        'typst_lsp',
+        'sqlls'
+      })
+      -- (Optional) Configure lua language server for neovim
+      local lua_opts = lsp_zero.nvim_lua_ls()
+      require('lspconfig').lua_ls.setup(lua_opts)
+      require 'lspconfig'.volar.setup {
+        init_options = {
+          typescript = {
+            tsdk = '/nix/store/yi14ijlcd02g1nsrf6izbhnwj9vj2lxx-typescript-5.1.6/lib/node_modules/typescript/lib'
+            -- Alternative location if installed as root:
+            -- tsdk = '/usr/local/lib/node_modules/typescript/lib'
+          }
         }
       }
-    })
-  })
-end
-
-return {
-  'VonHeikemen/lsp-zero.nvim',
-  branch = 'v2.x',
-  dependencies = {
-    -- LSP Support
-    { 'neovim/nvim-lspconfig' }, -- Required
-
-    -- Autocompletion
-    { 'hrsh7th/nvim-cmp',             lazy = true }, -- Required
-    { 'hrsh7th/cmp-nvim-lsp',         lazy = true }, -- Required
-    { 'L3MON4D3/LuaSnip',             lazy = true }, -- Required
-
-    { 'hrsh7th/cmp-buffer',           lazy = true },
-    { 'hrsh7th/cmp-cmdline',          lazy = true },
-    { 'hrsh7th/cmp-path',             lazy = true },
-    { 'saadparwaiz1/cmp_luasnip',     lazy = true }, -- for custom snippets.
-    { 'rafamadriz/friendly-snippets', lazy = true }, -- for custom snippets.
-  },
-  config = confn
+    end
+  }
 }
