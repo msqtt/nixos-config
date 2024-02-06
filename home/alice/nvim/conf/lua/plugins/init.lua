a23fbfe6d16deaf2c107854800cf504f71e443d8
msqtt <mosquito@email.cn>
2024-02-06 17:56:41 +0800

    add nvim plugin filter wrong suffix

--- home/alice/nvim/conf/lua/plugins/init.lua
+++ home/alice/nvim/conf/lua/plugins/init.lua
@@ -2,7 +2,7 @@
 -- the table structure: https://github.com/folke/lazy.nvim#-plugin-spec
 local fp = require("utils").fp
 
-local current_dir = vim.fn.stdpath("config").."/lua/plugins"
+local current_dir = vim.fn.stdpath("config") .. "/lua/plugins"
 
 local dir = vim.fs.dir(current_dir)
 if dir == nil then
@@ -11,9 +11,9 @@ if dir == nil then
 end
 
 local files = fp.filter(dir, function(name)
-    return name ~= 'init.lua'
+  return string.sub(name, -4) == '.lua' and name ~= 'init.lua'
 end)
 
 return fp.map(files, function(name)
-    return string.gsub(name, '%.lua', '')
+  return string.gsub(name, '%.lua', '')
 end)
